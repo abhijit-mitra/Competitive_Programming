85bc548f1c809b77223e2509bc127a381daf79bc
Abhijit Mitra <abhijitmitra.in@gmail.com>
2020-05-19 13:54:26 +0530

    added hoisting

--- trickyJs/2_hoisting.js
+++ trickyJs/2_hoisting.js
@@ -2,5 +2,35 @@ function((){
   var a = b = 1;
 })();
 
-console.log('a', a); // undefined
+console.log('a', a); //Error: Uncaught ReferenceError: a is not defined
 console.log('b', b); // 1
+
+console.log(x) //undefined;
+var x = 2;
+
+
+var test = 1;
+function test(){};
+console.log(test); // 1
+
+
+console.log(test2()); // 2
+function test2(){return 2;}
+
+
+console.log(test3()); // Error: test3 is not a function
+var test3 = function(){return 3;}
+
+
+//Very Important
+
+var test4 = 1;
+
+function test4_1(){
+  test4 = 100;
+  return;
+  function test4(){};
+};
+
+test4_1();
+console.log(test4); // Output: 1
