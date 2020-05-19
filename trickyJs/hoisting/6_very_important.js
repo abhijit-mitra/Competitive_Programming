c1cac1bff293f8f6ee49488283524f6e639a4679
Abhijit Mitra <abhijitmitra.in@gmail.com>
2020-05-19 20:27:53 +0530

    added viq

--- trickyJs/hoisting/6_very_important.js
+++ trickyJs/hoisting/6_very_important.js
@@ -1,12 +1,37 @@
 //Very Important
 
-var test4 = 1;
+var test = 1;
 
-function test4_1(){
-  test4 = 100;
+function x(){
+  test = 100;
   return;
-  function test4(){};
+  function test(){};
 };
 
-test4_1();
-console.log(test4); // Output: 1
+x();
+console.log(test); // Output: 1
+
+
+//Very Important
+var test2 = 1;
+
+function test2(){
+  test2 = 100;
+  return;
+  function test2(){};
+};
+
+test2();
+console.log(test2); // Output: Error test2 is not a function
+
+/*
+JS parsing:
+  Stage 1 called Compilation phase:
+  test2=undefined;
+  test2=[function]
+
+  Stage 2 called Execution phase:
+  test2=1;
+  test2() call will fail as test2 is no more a function in execution phase
+
+*/
