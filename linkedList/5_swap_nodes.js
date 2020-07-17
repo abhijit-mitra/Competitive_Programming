20750a31586d9ce470e6220650b8086f152a0083
Abhijit Mitra <abhijitmitra@locus.sh>
2020-07-17 20:41:02 +0530

    added swap nodes

--- linkedList/5_swap_nodes.js
+++ linkedList/5_swap_nodes.js
@@ -9,7 +9,7 @@ LinkedList.append('E');
 const search = (value)=>{
     let curNode = LinkedList.head;
     let prevNode = null;
-    while(curNode !== value){
+    while(curNode.data !== value){
         prevNode = curNode;
         curNode = curNode.next;
     };
@@ -19,6 +19,31 @@ const search = (value)=>{
     }
 }
 
-const {prevNode: prevNode_1, curNode:curNode_1 } =search('B');
-const {prevNode, curNode} =search('D');
+let {prevNode: prevNode_1, curNode:curNode_1 } =search('A');
+let {prevNode:prevNode_2 , curNode: curNode_2} =search('E');
+
+const temp = {...curNode_2};
+
+if(prevNode_1){
+    if(prevNode_1.data !== curNode_2.data){
+        prevNode_1.next = curNode_2;
+    }
+    curNode_2.next = curNode_1.next; 
+    
+}else{
+    LinkedList.head = curNode_2;
+    curNode_2.next = curNode_1;
+}
+
+if(prevNode_2){
+    if(prevNode_2.data !== curNode_1.data){
+        prevNode_2.next = curNode_1;
+    }
+    curNode_1.next = temp.next;
+}else{
+    LinkedList.head = curNode_1;
+    curNode_1.next = curNode_2;
+}
+
+console.log(LinkedList.print());
 
