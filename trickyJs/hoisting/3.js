b86dcfd258fe5ac9f5e055f74c22779101e79468
Abhijit Mitra <abhijitmitra.in@gmail.com>
2022-02-28 14:07:16 +0530

    Update 3.js

--- trickyJs/hoisting/3.js
+++ trickyJs/hoisting/3.js
@@ -2,6 +2,10 @@ function((){
   var a = b = 1;
 })();
 
-console.log('a', a); //Error: Uncaught ReferenceError: a is not defined
-console.log('b', b); // 1
-console.log('window.b', window.b); // 1
+console.log('a', a); 
+console.log('b', b);
+console.log('window.b', window.b); 
+
+//Error: Uncaught ReferenceError: a is not defined
+// 1
+// 1
