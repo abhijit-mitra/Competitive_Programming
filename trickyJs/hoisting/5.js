00e3c744fe99f2712b0ceaa43dd5848adea25b06
Abhijit Mitra <abhijitmitra.in@gmail.com>
2022-02-28 14:08:51 +0530

    Update 5.js

--- trickyJs/hoisting/5.js
+++ trickyJs/hoisting/5.js
@@ -1,7 +1,15 @@
-console.log(test2()); // 2
-function test2(){return 2;} // function declaration get hoisted
+console.log(test2()); 
+function test2(){return 2;} 
+
+// 2
+// function declaration get hoisted
 
 
-console.log(test3); // undefined
-console.log(test3()); // Error: test3 is not a function
-var test3 = function(){return 3;} // function expression not get hoisted
+
+console.log(test3); 
+console.log(test3()); 
+var test3 = function(){return 3;} 
+
+// undefined
+// Error: test3 is not a function
+// function expression not get hoisted
