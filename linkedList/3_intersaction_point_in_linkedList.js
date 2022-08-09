fc0cd05ed023f38d4dfbbb3c39f2df6d6050c9aa
Abhijit Mitra <abhijitmitra.in@gmail.com>
2022-08-09 22:58:13 +0530

    Update 3_intersaction_point_in_linkedList.js

--- linkedList/3_intersaction_point_in_linkedList.js
+++ linkedList/3_intersaction_point_in_linkedList.js
@@ -18,3 +18,11 @@ let list_2={
 
   }
 }
+
+//Check length of both linked list , const Size1 and const Size2
+// get the absolute difference of length of both linked list, diff = abs(Size1 - Size2)
+// if(Size1>Size2){ do LinkedList1.next diff times}
+// else{ do LinkedList2.next diff times}
+// while(LinkedList1!==LinkedList2){ LinkedList1 = Linkedlist1.next; LinkedList2 = LinkedList2.next;}
+// if(LinkedList1){ return LinkedList1.value}
+// return -1
