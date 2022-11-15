6697a998609bd0529301b5d277b4846558471a00
Abhijit Mitra <abhijitmitra.in@gmail.com>
2022-11-15 12:21:15 +0530

    Update 3_bind.js

--- javascriptUtils/3_bind.js
+++ javascriptUtils/3_bind.js
@@ -1,9 +1,32 @@
-Function.prototype.bind = function(){
-  const method = this;
-  const context = arguments[0];
-  const args = arguments.slice(1);
-  return function(){
-    const args_2 = arguments;
-    return method.apply(context, [...args, ...args_2]);
-  }
+const obj = {
+  firstName: "sds",
+  lastName: "bh"
+};
+
+function getFullName(state) {
+  return this.firstName + " " + this.lastName + " " + state;
 }
+
+Function.prototype.myBind = function (obj, ...args) {
+  obj.func = this;
+  return () => {
+    return obj.func(...args);
+  };
+};
+
+Function.prototype.myCall = function (obj, ...args) {
+  obj.func = this;
+  return obj.func(...args);
+};
+
+Function.prototype.myApply = function (obj, args) {
+  obj.func = this;
+  return obj.func(...args);
+};
+
+const fName2 = getFullName.myBind(obj, "bsh");
+console.log(fName2()); //sds bh bsh
+
+console.log(getFullName.myCall(obj, "kkr"));  //sds bh kkr
+
+console.log(getFullName.myApply(obj, ["kkr"]));  //sds bh kkr
